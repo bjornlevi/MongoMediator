6336ee2803a030e256d0fd3c36f94a66129a7bb3
Björn Leví Gunnarsson <bjornlevi@gmail.com>
2014-11-13 10:47:03 +0000

    adding before and after handlers

---
+++ before_and_after_handlers.py
@@ -0,0 +1,38 @@
+import cherrypy
+import uuid
+
+def before_submit():
+	cherrypy.request.json["email_link"] = str(uuid.uuid1())
+
+def after_submit():
+	pass
+
+def before_update():
+	pass
+
+def after_update():
+	pass
+
+def before_delete():
+	pass
+
+def after_delete():
+	pass
+
+def before_get():
+	pass
+
+def after_get():
+	pass
+
+def before_get_where():
+	pass
+
+def after_get_where():
+	pass
+
+def before_get_all():
+	pass
+
+def after_get_all():
+	pass
