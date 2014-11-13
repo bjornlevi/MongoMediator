ef095ca6f12d7f535b10d820bef53155a3177abd
Björn Leví Gunnarsson <bjornlevi@gmail.com>
2014-11-13 10:49:40 +0000

    handlers into app and updated validation functions

--- app.py
+++ app.py
@@ -4,15 +4,30 @@ from DatabasePlugin import DatabasePlugin
 
 from db import Order
 import input_validation
+import before_and_after_handlers as handlers
 
-cherrypy.tools.validate = cherrypy.Tool('before_handler', input_validation.validate)
-cherrypy.tools.validate_any = cherrypy.Tool('before_handler', input_validation.validate_any)
+cherrypy.tools.validate_and = cherrypy.Tool('before_handler', input_validation.validate_and)
+cherrypy.tools.validate_or = cherrypy.Tool('before_handler', input_validation.validate_or)
+cherrypy.tools.before_submit = cherrypy.Tool('before_handler', handlers.before_submit)
+cherrypy.tools.after_submit = cherrypy.Tool('before_handler', handlers.after_submit)
+cherrypy.tools.before_update = cherrypy.Tool('before_handler', handlers.before_update)
+cherrypy.tools.after_update = cherrypy.Tool('before_handler', handlers.after_update)
+cherrypy.tools.before_delete = cherrypy.Tool('before_handler', handlers.before_delete)
+cherrypy.tools.after_delete = cherrypy.Tool('before_handler', handlers.after_delete)
+cherrypy.tools.before_get = cherrypy.Tool('before_handler', handlers.before_get)
+cherrypy.tools.after_get = cherrypy.Tool('before_handler', handlers.after_get)
+cherrypy.tools.before_get_where = cherrypy.Tool('before_handler', handlers.before_get_where)
+cherrypy.tools.after_get_where = cherrypy.Tool('before_handler', handlers.after_get_where)
+cherrypy.tools.before_get_all = cherrypy.Tool('before_handler', handlers.before_get_all)
+cherrypy.tools.after_get_all = cherrypy.Tool('before_handler', handlers.after_get_all)
 
 class Controller(object):
 	@cherrypy.expose
 	@cherrypy.tools.json_in()
 	@cherrypy.tools.json_out()
-	@cherrypy.tools.validate(data_structure=input_validation.submit)
+	@cherrypy.tools.validate_and(data_structure=input_validation.submit)
+	@cherrypy.tools.before_submit()
+	@cherrypy.tools.after_submit()
 	@cherrypy.tools.allow(methods=['POST'])
 	def submit(self):
 		return cherrypy.engine.publish('db-save', cherrypy.request.json)
@@ -20,7 +35,9 @@ class Controller(object):
 	@cherrypy.expose
 	@cherrypy.tools.json_in()
 	@cherrypy.tools.json_out()
-	@cherrypy.tools.validate(data_structure=input_validation.update)
+	@cherrypy.tools.validate_and(data_structure=input_validation.update)
+	@cherrypy.tools.before_update()
+	@cherrypy.tools.after_update()
 	@cherrypy.tools.allow(methods=['PUT'])
 	def update(self):
 		update_id = cherrypy.request.json["update_id"]
@@ -30,27 +47,35 @@ class Controller(object):
 	@cherrypy.expose
 	@cherrypy.tools.json_in()
 	@cherrypy.tools.json_out()
-	@cherrypy.tools.validate(data_structure=input_validation.delete)
+	@cherrypy.tools.validate_or(data_structure=input_validation.delete)
+	@cherrypy.tools.before_delete()
+	@cherrypy.tools.after_delete()
 	@cherrypy.tools.allow(methods=['POST'])
 	def delete(self):
 		return cherrypy.engine.publish('db-delete', cherrypy.request.json)
 
 	@cherrypy.expose
 	@cherrypy.tools.json_out()
+	@cherrypy.tools.before_get()
+	@cherrypy.tools.after_get()
 	@cherrypy.tools.allow(methods=['GET'])
-	def get(self, order_id):
-		return cherrypy.engine.publish('db-get', order_id)
+	def get(self, _id):
+		return cherrypy.engine.publish('db-get', _id)
 
 	@cherrypy.expose
 	@cherrypy.tools.json_in()
 	@cherrypy.tools.json_out()
-	@cherrypy.tools.validate(data_structure=input_validation.get_where)
+	@cherrypy.tools.validate_or(data_structure=input_validation.get_where)
+	@cherrypy.tools.before_get_where()
+	@cherrypy.tools.after_get_where()
 	@cherrypy.tools.allow(methods=['POST'])
 	def get_where(self):
 		return cherrypy.engine.publish('db-getwhere', cherrypy.request.json)
 
 	@cherrypy.expose
 	@cherrypy.tools.json_out()
+	@cherrypy.tools.before_get_all()
+	@cherrypy.tools.after_get_all()
 	@cherrypy.tools.allow(methods=['GET'])
 	def get_all(self):
 		return cherrypy.engine.publish('db-getall')
--- input_validation.py
+++ input_validation.py
@@ -1,32 +1,37 @@
 import cherrypy
 
 
-def validate(data_structure):
-	input_structure = cherrypy.request.json
+def validate_and(data_structure):
 	missing = []
 	for key in data_structure:
-		if key not in input_structure:
+		if key not in cherrypy.request.json:
 			missing.append(key)
 
 	extras = []
-	for key in input_structure:
+	for key in cherrypy.request.json:
 		if key not in data_structure:
 			extras.append(key)
 
+	#There can be no missing keys and no extra keys
 	if len(missing) > 0 or len(extras) > 0:
 		raise cherrypy.HTTPError("400 Bad Request", "Required fields are: " + str(data_structure))
 
-def validate_any(data_structure):
-	input_structure = cherrypy.request.json
+def validate_or(data_structure):
+	found = []
+	for key in data_structure:
+		if key in cherrypy.request.json:
+			found.append(key)
+
 	extras = []
-	for key in input_structure:
+	for key in cherrypy.request.json:
 		if key not in data_structure:
 			extras.append(key)
 
-	if len(extras) > 0:
-		raise cherrypy.HTTPError("400 Bad Request", "Allowed fields are: " + str(data_structure))
+	#No keys found or extra keys in request 
+	if len(found) == 0 or len(extras) > 0:
+		raise cherrypy.HTTPError("400 Bad Request", "Required fields are: " + str(data_structure))
 
-submit = ["_id"]
+submit = ["email", "text"]
 update = ["_id"]
 delete = ["_id"]
 get_where = ["_id"]
